d36a205e96836d2613993fecc58fa01a9243f972
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2019-11-12 12:24:44 -0600

    kdevelop: update

--- scripts/develop.sh
+++ scripts/develop.sh
@@ -7,7 +7,7 @@ DESTDIR=$2
 
 for c in grep cmp sha1sum make tar git
 do
-    if ! command -v $c > /dev/null 
+    if ! command -v $c > /dev/null
     then
         echo >&2 "The script needs the \"$c\" command, and it was not found."
         exit 1
@@ -23,6 +23,8 @@ fi
 if [ -z "${DESTDIR}" ]
 then
     DESTDIR=.
+else
+    mkdir -p ${DESTDIR}
 fi
 
 SRC_URL=$(grep "^Source0:" "${SPECFILE}" | cut -f 2- -d ':' | tr -d " ")
@@ -33,12 +35,16 @@ SRC_VER=${SRC_DIR#*-}
 if [ ! -f ${SRC_FILE} ]
 then
     # Get upstream sources
-    make sources
+    if ! curl --fail -LO ${SRC_URL}
+    then
+        echo >&2 "Cannot download ${SRC_FILE}"
+        exit 3
+    fi
 fi
 
 echo $(sha1sum ${SRC_FILE} | cut -d\  -f1)/${SRC_FILE} > upstream.check
 
-if ! cmp --quiet upstream upstream.check 
+if ! cmp --quiet upstream upstream.check
 then
     echo >&2 "${SRC_FILE} checksum fails"
     rm upstream.check
@@ -56,13 +62,20 @@ git -C ${DESTDIR}/${SRC_DIR} add --all
 git -C ${DESTDIR}/${SRC_DIR} commit -m "${PKG_NAME} ${SRC_VER}" --quiet
 git -C ${DESTDIR}/${SRC_DIR} tag -a -m "v${SRC_VER}" "v${SRC_VER}"
 
-for p in CVE* [0-9]*.patch 
+for p in CVE* [0-9]*.patch
 do
-    git -C ${DESTDIR}/${SRC_DIR} am --quiet $(realpath $p)
+    if [ -f $p ]
+    then
+        if ! git -C ${DESTDIR}/${SRC_DIR} am --quiet $(realpath $p)
+        then
+            echo >&2 "Error at: ${p}"
+            exit 4
+        fi
+    fi
 done
 
 cp config ${DESTDIR}/${SRC_DIR}/.config
 
-echo 
+echo
 echo "The linux source plus Clear Linux patches is"
 echo "placed at: \"$(realpath ${DESTDIR}/${SRC_DIR})\""
